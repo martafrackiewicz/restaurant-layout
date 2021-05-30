d23a002dcc9c1de65e615959e64a207124097c70
Marta Frąckiewicz <frackiewiczmarta2@gmail.com>
2021-05-30 20:10:07 +0200

    Add carousel functions

---
+++ src/script.js
@@ -0,0 +1,47 @@
+// -----------------------carousel----------------------------------
+const $slides = document.querySelectorAll(".carousel-slide");
+const $leftArrow = document.querySelector(".carousel-arrow-left");
+const $rightArrow = document.querySelector(".carousel-arrow-right");
+
+let index = 0;
+
+const moveRight = () => {
+  if (index == $slides.length - 1) {
+    index = 0;
+  } else {
+    index++;
+  }
+
+  setSlide(index);
+  clearInterval(slideInterval);
+};
+
+const moveLeft = () => {
+  if (index == 0) {
+    index = $slides.length - 1;
+  } else {
+    index--;
+  }
+
+  setSlide(index);
+  clearInterval(slideInterval);
+};
+
+const setSlide = (index) => {
+    $slides.forEach((slide, i) => {
+      if (slide.classList.contains("active")) {
+        slide.classList.remove("active");
+      }
+      if (index === i) {
+          slide.classList.add("active");
+      }
+    });
+}
+
+$leftArrow.addEventListener("click", moveLeft);
+$rightArrow.addEventListener("click", moveRight);
+
+const slideInterval = setInterval(() => {
+  moveRight();
+}, 2000);
+// ----------------------end-of-carousel------------------------------
